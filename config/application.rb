c4adeec45f723a89f14e593dc63342cab9a55ff6
Rafael Espinoza <rafael@rafaelespinoza.com>
2016-03-20 13:03:27 -0700

    Use db/structure.sql for DDL instead of db/schema.rb
    This is for properly documenting the enum datatype I'll be using in the
    'meetings' table. It's a feature that is only for Postgres and the schema
    generator does not recognize it, making 'rake db:reset' problematic.

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module MusicSchool
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Use PostgreSQL instead of Ruby schema because we're using 'enum' datatype
+    config.active_record.schema_format = :sql
   end
 end
