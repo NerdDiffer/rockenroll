32b1d4142928c4ccf5a298b40c0b44e94d0af491
Rafael Espinoza <rafael@rafaelespinoza.com>
2016-03-24 21:43:21 -0700

    Add validation & two instance methods to Meeting model

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -10,4 +10,29 @@ class Meeting < ActiveRecord::Base
     seventy_five_minute: '75',
     ninety_minute:       '90'
   }
+
+  validate :start, :acceptable_starting_time?
+
+  def stop
+    return unless start?
+    return unless length?
+
+    start + length_to_i.minutes
+  end
+
+  def length_to_i
+    self.class.lengths[length].to_i
+  end
+
+  private
+
+  def acceptable_starting_time?
+    message =  'The minute in the starting time must be a 0 or a 5. ie: '
+    message += '3:00, 3:05, 3:10, 3:15, 3:20, 3:25, 3:30 etc.'
+    errors.add(:start, message) unless divisible_by_5
+  end
+
+  def divisible_by_5
+    start.min % 5 == 0
+  end
 end
