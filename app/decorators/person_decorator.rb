8e2e4d5912b3d3ab8030d63515392ce8bf07d090
Rafael Espinoza <rafael@rafaelespinoza.com>
2016-03-28 20:10:54 -0700

    Add a new people views & some methods to PersonDecorator
    Inside of the 'people/show' view, I moved the wrapping div & its header
    for associated enrollments inside of the partial. This is to match the
    pattern established by the new '_teachers' and '_students' partial.
    
    The reason for keeping the wrapping div & header inside of the partials
    for teachers & students is because a person may not always have a
    student or a teacher. It's cleaner this way. Though the individual
    partials could be DRYed up in the future.

--- app/decorators/person_decorator.rb
+++ app/decorators/person_decorator.rb
@@ -8,4 +8,20 @@ class PersonDecorator < Draper::Decorator
   def first_name_plus_initial
     first_name + ' ' + last_name[0] + '.'
   end
+
+  def show_enrollments
+    if enrollments.any?
+      h.render 'enrollments'
+    else
+      h.content_tag :p, "There are no enrollments for #{first_name}."
+    end
+  end
+
+  def show_students
+    h.render 'students' if students.any?
+  end
+
+  def show_teachers
+    h.render 'teachers' if teachers.any?
+  end
 end
